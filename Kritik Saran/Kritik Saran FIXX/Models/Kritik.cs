b9befba6d192b4c64ad1084f02fb6bfda96550ed
rrshandik <112605045+rrshandik@users.noreply.github.com>
2023-06-24 09:31:57 +0700

    Add files via upload

---
+++ Kritik Saran/Kritik Saran FIXX/Models/Kritik.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kritik_Saran_FIXX.Models
+{
+    internal class Kritik
+    {
+        public string kritikan { get; set; }
+    }
+}
